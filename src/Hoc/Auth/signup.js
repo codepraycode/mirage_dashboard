b327d496dbf574f3ee12ad197e9def6010baf9ba
CybertronicEvidence <adejuwonevidence181@gmail.com>
2021-11-21 20:40:42 +0100

    I've created the signup page

--- src/Hoc/Auth/signup.js
+++ src/Hoc/Auth/signup.js
@@ -58,10 +58,22 @@ class SignUp extends Component {
 
 
                         <div className="right card">
-                            {/* Add your code here */}
+                        <form action="#" method="post">
+                                    <p className="required">First name:</p>
+                                    <input type="text" name="firstname" placeholder="First name" minLength="1" maxLength="255"/>
+                                    <p className="required">Last name:</p>
+                                    <input type="text" name="lastname" placeholder="Last name" minLength="1" maxLength="255"/>
+                                    <p className="required">Email:</p>
+                                    <input type="text" name="email" placeholder="Email" minLength="1" maxLength="255"/>
+                                    <p className="required">Username:</p>
+                                    <input type="text" name="username" placeholder="Username" minLength="1" maxLength="255"/>
+                                    <p className="required">Password:</p>
+                                    <input type="password" name="password" placeholder="Password" minLength="6" maxLength="68" />
+                           <div className="text-center mv-1">       
+                            <button type="submit" className="btn btn-primary text-center">SUBMIT</button>
+                           </div>  
+                        </form>
 
-
-                            <button type="submit" className="btn btn-primary">Submit</button>
                         </div>
                     </div>
             </AuthWrapper>
