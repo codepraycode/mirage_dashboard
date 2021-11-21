7e9040332191b763f73313b6a7de0ebbfb3ef43e
CybertronicEvidence <adejuwonevidence181@gmail.com>
2021-11-21 20:21:54 +0100

    I disabled the media query in components.scss and global.scss because it was affecting my code

--- src/Hoc/Auth/login.js
+++ src/Hoc/Auth/login.js
@@ -6,17 +6,18 @@ class Login extends Component {
             <AuthWrapper signup={false} extraClass={"login"}>
                 
                         <div className="card">
-                            {/* 
-                                Replace 
-                                
-                                <p className="text-muted text-center">Login</p> 
+                        <form action="#" method="post">
+                                    <p className="required">Username:</p>
+                                    <input type="text" name="username" placeholder="Username" minLength="1" maxLength="255"/>
+                                    <p className="required">Password:</p>
+                                    <input type="password" name="password" placeholder="Password" minLength="6" maxLength="68"/><br/>
+                           <div className="text-center mv-1">       
+                            <button type="submit" className="btn btn-primary text-center">LOGIN</button>
+                           </div>  
+                        </form>
 
-                                with your code
-                                
-                             */}
-                            <p className="text-muted text-center">Login</p>
 
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            
                         </div>
                 
             </AuthWrapper>
