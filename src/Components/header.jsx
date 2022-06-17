64be369a49b7ca5e2981934349e5c689c66469b7
Precious Olusola <preciousolusola16@gmail.com>
2022-06-17 01:19:01 +0100

    updated overview header

--- src/Components/header.jsx
+++ src/Components/header.jsx
@@ -4,26 +4,27 @@ function Header() {
     return (
         <header>
             <nav className="navbar sticky">
-            <div className="nav__property">
-                <a href="/" className="logo">
+            <div className="nav_item">
+                <a href="/" className="brand">
                     {/* <span className="brand-logo" style={{background:`url('/asset/logo.svg') center center no-repeat`}}></span> */}
                     Mirage
                 </a>
                 
             </div>
 
-            <div className="nav__user">
-                <span className="menu-bar">
-                    <div className="icon-nav-toggle"></div>
-                    
-                </span>
+            <div className="nav_item">
+                
+                <div className="icons">
+                    <div className="icon-nav-toggle icon"></div>
+                </div>
 
                 
 
 
                 <span className="avatar__container">
+                    <span>codepraycode</span>
                     <div className="user__avatar">
-                        <img src="/asset/img/avatar.svg" alt="avatar"/>
+                        {/* <img src="/asset/img/avatar.svg" alt="avatar"/> */}
                     </div>
 
                 </span>
