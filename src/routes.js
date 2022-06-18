31ad143eebf1353272fa4fa5443e2c695ddd7ca3
Precious Olusola <preciousolusola16@gmail.com>
2022-06-18 05:53:21 +0100

    cleanup files

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,13 @@
 import React,{Suspense} from 'react';
 import {Routes , Route} from 'react-router-dom';
-import Layout from './Hoc/Layout/layout';
+
 // High Order Component
-import Layout2 from './Hoc/Layout/layout2';
+import Layout from './Hoc/Layout/layout';
 import SchoolLayout from './Hoc/Layout/school';
 
-// import NavLayout from './Hoc/Layout/navLayout';
-// import Dashboard from './Components/Dashboard/dashboard';
 import Page404 from './Components/Errors/404';
 import CircleLoader from './widget/preloader/circle';
 
-// const Overview = React.lazy(() => import('./Components/Overview/overview'));
-
-// const Staff = React.lazy(() => import('./Components/Staffs/staff'));
-// const Settings = React.lazy(() => import('./Components/settings/settings'));
 const NewSchool = React.lazy(() => import('./Components/Dashboard/new_school'));
 
 
@@ -44,7 +38,7 @@ const AppRoutes = () => {
 
             
 
-            <Route path="/" exact element={<Layout2/>}>
+            <Route path="/" exact element={<Layout/>}>
                 
                 <Route path="" index element={
                     <Suspense fallback={<CircleLoader/>}>
@@ -80,24 +74,6 @@ const AppRoutes = () => {
             </Route>
 
 
-            
-            
-
-
-            
-            
-            {/* <Route path="/signup" element={
-                <Suspense fallback={<CircleLoader/>}>
-                    <SignUp/>
-                </Suspense>
-            }/>
-
-            
-            <Route path="/login" element={
-                <Suspense fallback={<CircleLoader/>}>
-                    <Login/>
-                </Suspense>
-            }/> */}
             <Route path="/school/new" element={
                 <Suspense fallback={<CircleLoader/>}>
                     <Layout>
@@ -105,35 +81,7 @@ const AppRoutes = () => {
                     </Layout>
                 </Suspense>
             }/>
-            {/* <Route path="/school/:key" element={
-                <Suspense fallback={<CircleLoader/>}>
-                    <Layout>
-                        <NavLayout>
-                            <Overview/>
-                        </NavLayout>
-                    </Layout>
-                </Suspense>
-            }/> */}
-
-            {/* <Route path="/school/:key/staffs" element={
-                <Suspense fallback={<CircleLoader/>}>
-                    <Layout>
-                        <NavLayout>
-                        <Staff/>
-                        </NavLayout>
-                    </Layout>
-                </Suspense>
-            }/>
-
-            <Route path="/school/:key/settings" element={
-                <Suspense fallback={<CircleLoader/>}>
-                    <Layout>
-                        <NavLayout>
-                        <Settings/>
-                        </NavLayout>
-                    </Layout>
-                </Suspense>
-            }/> */}
+            
 
             
 
