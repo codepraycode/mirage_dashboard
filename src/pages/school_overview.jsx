bf13e753173f863b306a240648b0888e97dbe824
Precious Olusola <preciousolusola16@gmail.com>
2022-06-18 02:28:25 +0100

    added the credentials component

--- src/pages/school_overview.jsx
+++ src/pages/school_overview.jsx
@@ -9,8 +9,38 @@ const SchoolOverView = () => {
     <>
       <div className="container">
         <div className="row">
+          
           <div className="col">
-            <p>Details</p>
+            <div className="mb-3">
+              <p className="mb-1">Credentials</p>
+
+              <div className="box bg-default">
+                
+                <div className="p-10 w-40 bg-white flex align-center">
+                  
+                  <div className="text-primary p-10 mr-1 text-center">
+                    <i className="fas fa-key"></i>
+                  </div>
+
+                  <span> School Key</span>
+                </div>
+
+                <p className="w-60 text-right">
+              
+                  <span >
+                    dslfjkdaflsdkfjl
+                  </span>
+
+                  <span className="mx-2 safe copy-icon" onClick={()=>{}}>
+                    <i className="fas fa-copy"></i>
+                  </span>
+                  
+              
+                </p>
+
+              </div>
+
+            </div>
           </div>
 
           <div className="col pl-30">
